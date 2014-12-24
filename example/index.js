9d6204137fa1024abc2f58a1cf88e15adb362e9b
Eugene Song <tilleps@gmail.com>
2014-12-23 16:01:46 -0800

    Add related files and example

---
+++ example/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ *
+ * DEBUG=* node example/index.js 
+ * DEBUG=* DEBUG_LEVEL=error node example/index.js
+ */
+var levels = require('..').levels;
+var debug = require('..')('debug:example');
+
+debug.log('log!');
+debug.error('error!');
+debug.warn('warn!');
+debug.debug('debug!');
+debug.info('info!');
+debug.verbose('verbose!');
+
+
+//
+//  Display list of available levels
+//
+console.log('debug levels:', levels);
---
+++ index.js
@@ -0,0 +1,67 @@
+var debug = require('debug');
+
+var logLevel = process.env.DEBUG_LEVEL || 'verbose';
+var logLevels = [
+  'log',
+  'error',
+  'warn',
+  'debug',
+  'info',
+  'verbose'
+];
+
+
+//
+//  Empty function
+//
+function emptyFunction() {
+  return function () {};
+}
+
+
+//
+//  Check supported levels
+//
+var key = logLevels.indexOf(logLevel);
+
+if (key == -1) {
+  //  not in allowed levels
+  var msg = 'Log level found in allowed levels';
+  throw new Error(msg);
+}
+
+
+//
+//  Create a list of allowed levels
+//
+var allowedLevels = logLevels.slice(0, key + 1);
+
+
+/**
+ * Create Debug-level supported debug
+ * 
+ * @param {String} namespace
+ * @return {Object}
+ */
+module.exports = function (namespace) {
+  
+  var obj = {};
+  for (i in logLevels) {
+    
+    var logLevel = logLevels[i];    
+    var allowedLevel = allowedLevels.indexOf(logLevel) > -1;
+    
+    obj[logLevel] = allowedLevel 
+      ? debug(namespace)
+      : emptyFunction;
+
+  }
+  
+  return obj;
+}
+
+
+//
+//  Export available levels
+//
+module.exports.levels = logLevels;
